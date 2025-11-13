d6215c097c9c4644f10253c7e8595287a53e7348
KiwiSky <neonixbuilds@gmail.com>
2025-11-13 14:48:22 -0300

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<style>
+.container
+{
+    background-color: #260f3a;
+}
+
+.image-inicio
+{
+    width: 75%;
+    height: 75%;
+    border-radius: 25px;
+}
+
+.image-container
+{
+    display: flex;
+    justify-content: center;
+}
+
+.wave-footer
+{
+    aspect-ratio: 1920/1080;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
+
+.layer-footer
+{
+    height: 10%;
+    background-image: url('wave_footer.svg');
+}
+
+footer
+{
+    background-color: #47246d;
+}
+</style>
+
+<div class="container">
+    <div class="image-container">
+        <img class="image-inicio" src="sexo.png" alt="">
+    </div>
+
+    <div class="wave-footer layer-footer"></div>
+    <footer>
+
+    </footer>
+</div>
